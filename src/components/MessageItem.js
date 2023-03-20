522dca9b6c9ac661a948fbe0dd9143f4d077bb1b
hirutatsumi <tatsumi_hiruta@ex-ex.co.jp>
2023-03-20 15:10:03 +0900

    display messages

---
+++ src/components/MessageItem.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Avatar,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { gravartPath } from "../gravatar";
+
+const useStyles = makeStyles((theme) => ({
+  inline: {
+    display: "inline",
+  },
+}));
+
+const MessageItem = ({ name, text }) => {
+  const classes = useStyles();
+  const avatarPath = gravartPath(name);
+
+  return (
+    <ListItem divider={true}>
+      <ListItemAvatar>
+        <Avatar src={avatarPath} />
+      </ListItemAvatar>
+      <ListItemText
+        primary={name}
+        secondary={
+          <Typography
+            sx={{ display: "inline" }}
+            component="span"
+            variant="body2"
+            color="text.primary"
+          >
+            {text}
+          </Typography>
+        }
+      />
+    </ListItem>
+  );
+};
+
+export default MessageItem;
